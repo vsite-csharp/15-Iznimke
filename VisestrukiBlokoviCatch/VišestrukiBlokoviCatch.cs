b5b080b440ff5741731fd11c190cfa0b21b34557
Kresimir Bratkovic <kbratkovic@gmail.com>
2021-01-21 20:28:50 +0100

    Resolve multiple catch blocks

--- VisestrukiBlokoviCatch/VišestrukiBlokoviCatch.cs
+++ VisestrukiBlokoviCatch/VišestrukiBlokoviCatch.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
 
+
+
 namespace Vsite.CSharp.Iznimke
 {
     class VišestrukiBlokoviCatch
@@ -9,27 +11,49 @@ namespace Vsite.CSharp.Iznimke
         public const string FormatFiltraArgumentException = "Filtar ArgumentException: {0}";
         public const string FormatFiltraArgumentOutOfRangeException = "Filtar ArgumentOutOfRangeException: {0}";
 
+
+
         public static void HvatanjeIznimkePremaTipu(Exception iznimka)
         {
             try
             {
                 throw iznimka;
             }
-            // TODO:040 Složiti niz blokova catch tipa Exception, ArgumentOutOfRangeException i ArgumentException, dodati ispise koristeći gornje formate te provjeriti koja će iznimka biti uhvaćena u kojem bloku.
+            // Složiti niz blokova catch tipa Exception, ArgumentOutOfRangeException i ArgumentException, dodati ispise koristeći gornje formate te provjeriti koja će iznimka biti uhvaćena u kojem bloku.
             // https://docs.microsoft.com/en-us/dotnet/api/system.argumentoutofrangeexception
+
+
+
             catch (ArgumentOutOfRangeException e)
             {
                 Console.WriteLine(FormatFiltraArgumentOutOfRangeException, e.GetType().Name);
             }
+
+
+
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(FormatFiltraArgumentException, e.GetType().Name);
+            }
+
+
+
             catch (Exception e)
             {
                 Console.WriteLine(FormatFiltraException, e.GetType().Name);
             }
+
+
+
             Console.WriteLine();
         }
 
-        // TODO:042 Pokrenuti program i provjeriti ispise.
-        // TODO:043 Pokrenuti i provjeriti testove (4 testa u grupi "VišestrukiBlokoviCatch" moraju proći)
+
+
+        //  Pokrenuti program i provjeriti ispise.
+        //  Pokrenuti i provjeriti testove (4 testa u grupi "VišestrukiBlokoviCatch" moraju proći)
+
+
 
         static void Main(string[] args)
         {
@@ -40,10 +64,14 @@ namespace Vsite.CSharp.Iznimke
                 new ArgumentException(), new ArgumentNullException()
             };
 
+
+
             for (int i = 0; i < iznimke.Length; ++i)
             {
                 HvatanjeIznimkePremaTipu(iznimke[i]);
             }
+
+
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(false);
