c72a99e3f65ecde936f051186bd69410c0e603bb
Zvonimir Balen <72575824+zvbalen@users.noreply.github.com>
2021-01-21 19:52:35 +0100

    resolve exception throwing

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -6,8 +6,10 @@ namespace Vsite.CSharp.Iznimke
     {
         public static int Faktorjel(int broj)
         {
-            // TODO:003 Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
-            // TODO:004 Pokrenuti program i provjeriti što će se dogoditi.
+            //  Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
+            //  Pokrenuti program i provjeriti što će se dogoditi.
+            if (broj < 0)
+                throw new ArgumentOutOfRangeException(nameof(broj), "Argument ne smije biti negativan broj");
             int rezultat = 1;
             for (int i = 2; i <= broj; ++i)
                 rezultat *= i;
@@ -29,22 +31,49 @@ namespace Vsite.CSharp.Iznimke
 
         static void Main(string[] args)
         {
-            // TODO:000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
-            // TODO:001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
+            //  Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
+            // Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
             // Provjeriti opciju i za Debug i za Release!
-            // TODO:002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
+            // Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
 
-            // TODO:005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
-            // TODO:006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
-            // TODO:007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+            //  Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
+            //  U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
+            //  Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+            try
+            {
+                IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
+                IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
+                IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
+                IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
+                IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
 
-            IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
-            IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
-            IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
-            IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
-            IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+                Console.WriteLine($"e.Message: {e.Message}");
+                Console.WriteLine($"e.ParamName: {e.ParamName}");
+                Console.WriteLine($"e.Source: {e.Source}");
+                Console.WriteLine($"e.StackTrace: {e.StackTrace}");
+                Console.WriteLine($"e.TargetSite: {e.TargetSite}");
+                Console.WriteLine(e);
+            }
+            try
+            {
+               
+              IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
 
-            // TODO:008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
+                Console.WriteLine($"e.Message: {e.Message}");
+                Console.WriteLine($"e.ParamName: {e.ParamName}");
+                Console.WriteLine($"e.Source: {e.Source}");
+                Console.WriteLine($"e.StackTrace: {e.StackTrace}");
+                Console.WriteLine($"e.TargetSite: {e.TargetSite}");
+                Console.WriteLine(e);
+            }
+
+            //  Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(false);
