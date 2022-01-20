2e46db2cfd86277b4581e3b24a536bcbd25639d4
Robert Tušek <kratos365@gmail.hr>
2022-01-20 21:21:36 +0100

    Resolve exception filters

--- FilteriIznimki/FilteriIznimki.cs
+++ FilteriIznimki/FilteriIznimki.cs
@@ -25,20 +25,24 @@ namespace Vsite.CSharp.Iznimke
 
         public static void IspišiLogaritamBroja(double broj, double baza)
         {
-            // TODO:050 Pokrenuti program i provjeriti ispis.
-            // TODO:051 Dodati blok hvatanja s filtrom koji će hvatati ArgumentOutOfRangeException samo za broj <= 0 i u tom slučaju ispisati gornju poruku NedozvoljeniBroj.
+            // :050 Pokrenuti program i provjeriti ispis.
+            // :051 Dodati blok hvatanja s filtrom koji će hvatati ArgumentOutOfRangeException samo za broj <= 0 i u tom slučaju ispisati gornju poruku NedozvoljeniBroj.
             try
             {
                 Console.WriteLine(FormatIspisa, broj, baza, Math.Logaritam(broj, baza));
             }
+            catch (ArgumentOutOfRangeException e) when (broj < 0)
+            {
+                Console.WriteLine(NedozvoljeniBroj);
+            }   
             catch (ArgumentOutOfRangeException e)
             {
                 Console.WriteLine(FormatPogreške, e.ParamName, e.ActualValue);
             }
         }
 
-        // TODO:052 Pokrenuti program i provjeriti ispis.
-        // TODO:053 Pokrenuti i provjeriti testove (3 testa iz grupe "FiltriranjeIznimkiPredikatom" moraju proći)
+        // :052 Pokrenuti program i provjeriti ispis.
+        // :053 Pokrenuti i provjeriti testove (3 testa iz grupe "FiltriranjeIznimkiPredikatom" moraju proći)
 
         static void Main(string[] args)
         {
