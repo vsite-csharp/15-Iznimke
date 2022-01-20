0a838068f93349c72d57fc5a65b63dbe83ed80f7
Vedran Olujic <vedranolujic23@gmail.com>
2022-01-20 21:21:35 +0100

    resolved exception filters

--- TijekIzvodjenja/TijekIzvodjenja.cs
+++ TijekIzvodjenja/TijekIzvodjenja.cs
@@ -12,9 +12,9 @@ namespace Vsite.CSharp.Iznimke
 
         public static void IspisTryCatchFinally(int djeljenik, int djeljitelj)
         {
-            // TODO:030 Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
-            // TODO:031 Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
-            // TODO:032 Pokrenuti program i provjeriti što će se ispisati.
+            // 030 Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
+            // 031 Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
+            // 032 Pokrenuti program i provjeriti što će se ispisati.
 
             try
             {
