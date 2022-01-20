05726079a63f6c4b714180aef814f1916068f288
Vedran Olujic <vedranolujic23@gmail.com>
2022-01-20 20:41:20 +0100

    resolve exception throwing

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -8,6 +8,10 @@ namespace Vsite.CSharp.Iznimke
         {
             // TODO:003 Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
             // TODO:004 Pokrenuti program i provjeriti što će se dogoditi.
+
+            if (broj < 0)
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ne smije biti negativan broj.");
+            
             int rezultat = 1;
             for (int i = 2; i <= broj; ++i)
                 rezultat *= i;
@@ -29,21 +33,50 @@ namespace Vsite.CSharp.Iznimke
 
         static void Main(string[] args)
         {
-            // TODO:000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
-            // TODO:001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
+            // 000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
+            // 001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
             // Provjeriti opciju i za Debug i za Release!
-            // TODO:002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
+            // 002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
 
             // TODO:005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
             // TODO:006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
             // TODO:007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+            try
+            {
+                IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
+                IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
+                IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
+                IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
 
-            IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
-            IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
-            IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
-            IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
-            IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
 
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ParamName);
+                Console.WriteLine(e.ActualValue);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.TargetSite);
+                Console.WriteLine(e.StackTrace);
+
+                //throw e;
+            }
+            try
+            {
+
+                IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+
+
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source);
+                Console.WriteLine(e.TargetSite);
+                Console.WriteLine(e.StackTrace);
+
+                //throw e;
+            }
             // TODO:008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
 
             Console.WriteLine("GOTOVO!!!");
