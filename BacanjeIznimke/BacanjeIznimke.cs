93f015813a876313acf556f66de2d2d6335023fb
Ana Lovric <alovric@dspace.hr>
2022-01-20 20:41:11 +0100

    Resloved: Exception throwing

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -6,9 +6,13 @@ namespace Vsite.CSharp.Iznimke
     {
         public static int Faktorjel(int broj)
         {
-            // TODO:003 Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
-            // TODO:004 Pokrenuti program i provjeriti što će se dogoditi.
+            // 003 Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
+            // 004 Pokrenuti program i provjeriti što će se dogoditi.
             int rezultat = 1;
+            if (broj < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(broj),broj, "Argument ne smije biti negativni broj");
+            }
             for (int i = 2; i <= broj; ++i)
                 rezultat *= i;
             return rezultat;
@@ -29,22 +33,45 @@ namespace Vsite.CSharp.Iznimke
 
         static void Main(string[] args)
         {
-            // TODO:000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
-            // TODO:001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
+            // 000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
+            // 001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
             // Provjeriti opciju i za Debug i za Release!
-            // TODO:002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
+            // 002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
 
-            // TODO:005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
-            // TODO:006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
-            // TODO:007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+            // 005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
+            // 006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
+            // 007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+            try
+            {
+                IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
+                IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
+                IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
+                IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
+            }
+            catch (ArgumentOutOfRangeException e) //zelimo tocno odedeni tip iznimke - svjesni sto ocekujemo
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ParamName);
+                Console.WriteLine(e.ActualValue); //vrijednost koja je uzrokovala exception
+                Console.WriteLine(e.Source); //to je assembly, to je ime naseg projekta BacanjeIznimke, to je dll
+                Console.WriteLine(e.TargetSite);
+                Console.WriteLine(e.StackTrace);
+            }
 
-            IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
-            IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
-            IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
-            IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
-            IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+            //moramo odvojiti zadnju liniju koda jer ona izaziva drugu vratu execptiona
+            try
+            {
+                IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+            }
+            catch (OverflowException e) //zelimo tocno odedeni tip iznimke - svjesni sto ocekujemo
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Source); 
+                Console.WriteLine(e.TargetSite);
+                Console.WriteLine(e.StackTrace);
+            }
 
-            // TODO:008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
+            // 008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey(false);
