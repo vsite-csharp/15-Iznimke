7563b758208f8376ec552d180c78e28bd345c124
Ivan Čulina <72503859+ivculina@users.noreply.github.com>
2022-01-20 20:54:33 +0100

    Resolve exception abstraction

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -10,10 +10,20 @@ namespace Vsite.CSharp.Iznimke
             // :004 Pokrenuti program i provjeriti što će se dogoditi.
             if (broj < 0)
                 throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ne smije biti negativni broj");
-            int rezultat = 1;
-            for (int i = 2; i <= broj; ++i)
-                rezultat *= i;
-            return rezultat;
+            try
+            {
+                int rezultat = 1;
+                for (int i = 2; i <= broj; ++i)
+                    rezultat *= i;
+                return rezultat;
+            }
+            catch (OverflowException)
+            {
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument je preveliki broj");
+
+                throw;
+            }
+            
         }
         // ova funkcija se koristi kasnije
         public static int Povrh(int n, int k)
