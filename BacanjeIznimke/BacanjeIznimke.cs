667a3e7f10453e2a4cb9c4e49318a26c83bebd33
Ana Lovric <alovric@dspace.hr>
2022-01-20 20:54:26 +0100

    Resloved exception apstraction

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -8,14 +8,23 @@ namespace Vsite.CSharp.Iznimke
         {
             // 003 Dodati u metodu provjeru je li argument manji od 0 i u tom slučaju baciti iznimku tipa ArgumentOutOfRangeException s porukom: "Argument ne smije biti negativni broj"
             // 004 Pokrenuti program i provjeriti što će se dogoditi.
-            int rezultat = 1;
+
             if (broj < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(broj),broj, "Argument ne smije biti negativni broj");
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ne smije biti negativni broj");
             }
-            for (int i = 2; i <= broj; ++i)
-                rezultat *= i;
-            return rezultat;
+
+            try
+            {
+                int rezultat = 1;
+                for (int i = 2; i <= broj; ++i)
+                    rezultat *= i;
+                return rezultat;
+            }
+            catch (OverflowException e)
+            {
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument je prevelik broj");
+            }
         }
         // ova funkcija se koristi kasnije
         public static int Povrh(int n, int k)
@@ -66,7 +75,7 @@ namespace Vsite.CSharp.Iznimke
             catch (OverflowException e) //zelimo tocno odedeni tip iznimke - svjesni sto ocekujemo
             {
                 Console.WriteLine(e.Message);
-                Console.WriteLine(e.Source); 
+                Console.WriteLine(e.Source);
                 Console.WriteLine(e.TargetSite);
                 Console.WriteLine(e.StackTrace);
             }
