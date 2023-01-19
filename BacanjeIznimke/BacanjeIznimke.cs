ef2a59f7810cd29289c3a22afc33b556bd6b939b
ana.grlic <ana.grlic@UNIVULAP348>
2023-01-19 20:00:18 +0100

    resolve exception abastraction

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -11,60 +11,69 @@
             {
                 throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ne smije biti negativni broj");
             }
-
-            int rezultat = 1;
-            for (int i = 2; i <= broj; ++i)
-                rezultat *= i;
-            return rezultat;
-        }
-        // ova funkcija se koristi kasnije
-        public static int Povrh(int n, int k)
-        {
-            return Faktorjel(n) / (Faktorjel(k) * Faktorjel(n - k));
-        }
-    }
-
-    class BacanjeIznimke
-    {
-        static void IspišiFaktorjel(int n)
-        {
-            Console.WriteLine($"{n}! = {Math.Faktorjel(n)}");
-        }
-
-        static void Main(string[] args)
-        {
-            //000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
-            //001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
-            // Provjeriti opciju i za Debug i za Release!
-            //002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
-
-            //005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
-            //006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
-            //007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
-
             try
             {
-                IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
-                IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
-                IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
-                IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
-                IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+                int rezultat = 1;
+                for (int i = 2; i <= broj; ++i)
+                    rezultat *= i;
+                return rezultat;
+
             }
-            catch (ArgumentOutOfRangeException e)
+            catch (OverflowException)
             {
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument je prevelik");
+            }
+        }
 
-                Console.WriteLine($"ActualValue: {e.ActualValue}");
-                Console.WriteLine($"HResult: {e.HResult}");
-                Console.WriteLine($"Message: {e.Message}");
-                Console.WriteLine($"ParamName: {e.ParamName}");
-                Console.WriteLine($"Source: {e.Source}");
-                Console.WriteLine($"StackTrace: {e.StackTrace}");
-                Console.WriteLine($"TargetSite: {e.TargetSite}");
+            // ova funkcija se koristi kasnije
+            public static int Povrh(int n, int k)
+            {
+                return Faktorjel(n) / (Faktorjel(k) * Faktorjel(n - k));
+            }
+        
+        }
+
+        class BacanjeIznimke
+        {
+            static void IspišiFaktorjel(int n)
+            {
+                Console.WriteLine($"{n}! = {Math.Faktorjel(n)}");
             }
 
-            // TODO:008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
+            static void Main(string[] args)
+            {
+                //000 Pokrenuti program i provjeriti ispravnost ispisanih rezultata.
+                //001 Provjeriti u postavkama projekta (Properties-Build-Advanced) da se radi provjera na brojčani preljev (tj. da je uključena opcija "check for arithmetic overflow/underflow").
+                // Provjeriti opciju i za Debug i za Release!
+                //002 Pokrenuti program i provjeriti ispravnost ispisanih rezultata prije bacanja iznimke.
 
-            Console.WriteLine("\nGOTOVO!!!");
+                //005 Donje pozive funkcije Faktorjel staviti u blok try i iza njega hvatati iznimke tipa ArgumentOutOfRangeException.
+                //006 U bloku catch ispisati neka interesantna svojstva klase ArgumentOutOfRangeException.
+                //007 Provjeriti vraćaju li nakon promjena donji pozivi metode očekivane rezultate.
+
+                try
+                {
+                    IspišiFaktorjel(0); // trebalo bi ispisati: 0! = 1
+                    IspišiFaktorjel(3); // trebalo bi ispisati: 3! = 6
+                    IspišiFaktorjel(5); // trebalo bi ispisati: 5! = 120
+                    IspišiFaktorjel(-1); // trebalo bi baciti iznimku!
+                    IspišiFaktorjel(17); // trebalo bi baciti iznimku zbog preljeva!
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+
+                    Console.WriteLine($"ActualValue: {e.ActualValue}");
+                    Console.WriteLine($"HResult: {e.HResult}");
+                    Console.WriteLine($"Message: {e.Message}");
+                    Console.WriteLine($"ParamName: {e.ParamName}");
+                    Console.WriteLine($"Source: {e.Source}");
+                    Console.WriteLine($"StackTrace: {e.StackTrace}");
+                    Console.WriteLine($"TargetSite: {e.TargetSite}");
+                }
+
+                // TODO:008 Pokrenuti testove (svi testovi u grupi "BacanjeIznimke" moraju proći)
+
+                Console.WriteLine("\nGOTOVO!!!");
+            }
         }
     }
-}
