31f511ff28d10bbe3663d871fa575ef604d31b61
Fran Štambuk <fran.stambuk@gmail.com>
2023-01-19 20:09:09 +0100

    Resolved handling separation

--- OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
+++ OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
@@ -1,25 +1,32 @@
-﻿namespace Vsite.CSharp.Iznimke
+﻿using System.Security.Claims;
+
+namespace Vsite.CSharp.Iznimke
 {
     class OdvajanjeGlavneLogike
     {
         public static void IspisPovrh(int prvi, int zadnji)
         {
-            // TODO:020 Pokrenuti program i provjeriti što će se dogoditi.
-            // TODO:021 Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
+            // Pokrenuti program i provjeriti što će se dogoditi.
+            // Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
+            try
             {
                 for (int n = prvi; n < zadnji; ++n)
                 {
                     for (int k = 1; k <= n; ++k)
                     {
                         // pozivamo funkciju Povrh definiranu u projektu BacanjeIznimke
-                        Console.WriteLine($"{n} povrh {k} = {Math.Povrh(n, k)}");
+                       Console.WriteLine($"{n} povrh {k} = {Math.Povrh(n, k)}");
                     }
                 }
             }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
-        // TODO:022 Pokrenuti program i provjeriti ispis.
-        // TODO:023 Pokrenuti i provjeriti testove (test u grupi "OdvajanjaGlavneLogike" mora proći)
+        // Pokrenuti program i provjeriti ispis.
+        // Pokrenuti i provjeriti testove (test u grupi "OdvajanjaGlavneLogike" mora proći)
 
         static void Main(string[] args)
         {
