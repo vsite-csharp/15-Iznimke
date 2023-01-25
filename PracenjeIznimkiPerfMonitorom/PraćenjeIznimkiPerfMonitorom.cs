b39202e50cd940a91a607479d8669b09a08ac197
Dario Jakovljević <dajakovl@vsite.hr>
2023-01-25 14:48:17 +0100

    resolved rest of todos

--- PracenjeIznimkiPerfMonitorom/PraćenjeIznimkiPerfMonitorom.cs
+++ PracenjeIznimkiPerfMonitorom/PraćenjeIznimkiPerfMonitorom.cs
@@ -3,12 +3,12 @@
     // Program generira iznimke u određenim vremenskim intervalima da bismo
     // ih mogli pratiti u PERFMON aplikaciji
 
-    // TODO:110 Pokrenuti program izvan Visual Studija
+    // :110 Pokrenuti program izvan Visual Studija
     // TODO:111 Pokrenuti program PerfMon i u njemu uključiti praćenje .NET CLR Exceptions:
     //          - kliknuti na crveni + i u popisu brojača potražiti .NET CLR Exceptions
     //          - proširiti stavku .NET CLR Exceptions i u njoj odabrati "# of Exceps Thrown"
     //          - u popisu objekata potražiti i selektirati "IznimkePerfMon" te pritisnuti tipku Add.
-    // TODO:112 Kliknuti na tipku u formi naše aplikacije da počne bacati iznimke te pratiti graf u programu PerfMon.
+    // :112 Kliknuti na tipku u formi naše aplikacije da počne bacati iznimke te pratiti graf u programu PerfMon.
     static class PraćenjeIznimkiPerfMonitorom
     {
         /// <summary>
