99d1c9cbd846a78462ed4de1719d74f82a2b347c
Sonja Perković <soperkov@vsite.hr>
2024-01-25 20:20:27 +0100

    Create exception handler outside loops

--- OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
+++ OdvajanjeGlavneLogike/OdvajanjeGlavneLogike.cs
@@ -4,22 +4,30 @@
     {
         public static void IspisPovrh(int prvi, int zadnji)
         {
-            // TODO:020 Pokrenuti program i provjeriti što će se dogoditi.
-            // TODO:021 Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
+            // 020 Pokrenuti program i provjeriti što će se dogoditi.
+            // 021 Donje petlje obuhvatiti try-catch blokom koji će prekinuti daljnje računanje kada bude bačena iznimka. Unutar bloka hvatanja ispisati poruku o pogrešci.
             {
-                for (int n = prvi; n < zadnji; ++n)
+                try
                 {
-                    for (int k = 1; k <= n; ++k)
+                    for (int n = prvi; n < zadnji; ++n)
                     {
-                        // pozivamo funkciju Povrh definiranu u projektu BacanjeIznimke
-                        Console.WriteLine($"{n} povrh {k} = {Math.Povrh(n, k)}");
+                        for (int k = 1; k <= n; ++k)
+                        {
+                            // pozivamo funkciju Povrh definiranu u projektu BacanjeIznimke
+                            Console.WriteLine($"{n} povrh {k} = {Math.Povrh(n, k)}");
+                        }
                     }
+
+                }
+                catch(ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
                 }
             }
         }
 
-        // TODO:022 Pokrenuti program i provjeriti ispis.
-        // TODO:023 Pokrenuti i provjeriti testove (test u grupi "OdvajanjaGlavneLogike" mora proći)
+        // 022 Pokrenuti program i provjeriti ispis.
+        // 023 Pokrenuti i provjeriti testove (test u grupi "OdvajanjaGlavneLogike" mora proći)
 
         static void Main()
         {
