da4c56e679803b5a2ab0f01078221aff84ca7638
Patrik Kralj <patrikkralj97@gmail.com>
2024-01-25 20:15:19 +0100

    Handle overflow exception in factorial function

--- BacanjeIznimke/BacanjeIznimke.cs
+++ BacanjeIznimke/BacanjeIznimke.cs
@@ -8,13 +8,20 @@
             // :004 Pokrenuti program i provjeriti što će se dogoditi.
             if (broj < 0)
             {
-                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ni smije biti negativan broj");
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument ne smije biti negativan broj");
             }
 
-            int rezultat = 1;
-            for (int i = 2; i <= broj; ++i)
-                rezultat *= i;
-            return rezultat;
+            try
+            {
+                int rezultat = 1;
+                for (int i = 2; i <= broj; ++i)
+                    rezultat *= i;
+                return rezultat;
+            }
+            catch(OverflowException ex)
+            {
+                throw new ArgumentOutOfRangeException(nameof(broj), broj, "Argument je prevelik");
+            }
         }
         // ova funkcija se koristi kasnije
         public static int Povrh(int n, int k)
