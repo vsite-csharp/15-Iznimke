69bbac7994a6ccc2608ad1d1e274bce175f6c21c
Marko Pavić <mapavic@vsite.hr>
2024-01-25 20:28:39 +0100

    Check try-catch-finally blocks execution

--- TijekIzvodjenja/TijekIzvođenja.cs
+++ TijekIzvodjenja/TijekIzvođenja.cs
@@ -10,17 +10,31 @@
 
         public static void IspisTryCatchFinally(int djeljenik, int djeljitelj)
         {
-            // TODO:030 Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
-            // TODO:031 Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
-            // TODO:032 Pokrenuti program i provjeriti što će se ispisati.
+            // 030 Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
+            // 031 Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
+            // 032 Pokrenuti program i provjeriti što će se ispisati.
 
-            Console.WriteLine($"{djeljenik} dijelim s {djeljitelj}");
-            Console.WriteLine(djeljenik / djeljitelj);
-            Console.WriteLine($"{djeljenik} sam podijelio s {djeljitelj}");
+            try
+            {
+                Console.WriteLine(ZapočinjeBlokTry);
+                Console.WriteLine($"{djeljenik} dijelim s {djeljitelj}");
+                Console.WriteLine(djeljenik / djeljitelj);
+                Console.WriteLine($"{djeljenik} sam podijelio s {djeljitelj}");
+                Console.WriteLine(ZavršavaBlokTry);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(BlokCatch);
+            }
+            finally
+            {
+                Console.WriteLine(BlokFinally);
+            }
 
         }
 
-        // TODO:033 Pokrenuti i provjeriti rezultate testova (2 testa iz grupe "TijekIzvođenja" moraju proći).
+        // 033 Pokrenuti i provjeriti rezultate testova (2 testa iz grupe "TijekIzvođenja" moraju proći).
 
         static void Main()
         {
