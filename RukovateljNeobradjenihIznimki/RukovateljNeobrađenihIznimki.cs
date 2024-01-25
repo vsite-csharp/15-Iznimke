02981f2e3ebc813f18004825e8725ed880dbda9f
Mihael Krpes <mikrpes@vsite.hr>
2024-01-25 21:21:26 +0100

    unhanded exception

--- RukovateljNeobradjenihIznimki/RukovateljNeobrađenihIznimki.cs
+++ RukovateljNeobradjenihIznimki/RukovateljNeobrađenihIznimki.cs
@@ -6,7 +6,7 @@ namespace Vsite.CSharp.Iznimke
     // zapišemo ju u datoteku), no program će i nakon toga prekinuti izvođenje s neobrađenom iznimkom!
     static class RukovateljNeobrađenihIznimki
     {
-        public const string NeuhvaćenaIznimka = "Neuhvaćena iznimka: {0}";
+        public const string NeuhvaćenaIznimka = "NeuhvaćenaIznimka: {0}";
 
         private static void EvidentirajNeuhvaćenuIznimku(UnhandledExceptionEventArgs e)
         {
@@ -26,7 +26,7 @@ namespace Vsite.CSharp.Iznimke
             // TODO:071 Odkomentirati donju naredbu te događaju UnhandledException pridružiti rukovatelja (handlera) koji će pozvati gornju metodu ZapišiNeuhvaćenuIznimku.
             //          Pokrenuti program i provjeriti rezultat.
             // TODO:072 Provjeriti prolazi li test RukovateljNeobrađenihIznimki.
-            //AppDomain.CurrentDomain.UnhandledException +=
+            AppDomain.CurrentDomain.UnhandledException += ZapišiNeuhvaćenuIznimku;
             Exception[] iznimke = { new ArgumentOutOfRangeException(), new ArgumentNullException(), new DivideByZeroException() };
 
             foreach (var iznimka in iznimke)
@@ -43,6 +43,11 @@ namespace Vsite.CSharp.Iznimke
             }
 
             Console.WriteLine("\nGOTOVO!!!");
+        }
+
+        private static void ZapišiNeuhvaćenuIznimku(object sender, UnhandledExceptionEventArgs e)
+        {
+            EvidentirajNeuhvaćenuIznimku(e);
         }
     }
 }
