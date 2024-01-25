8a653f9f3bae636bc573db7ebb6a7d38d85feed5
benjamin.gajski@gmail.com <Benjamin.Gajski@gmail.com>
2024-01-25 20:25:42 +0100

    03 - tijek izvođenja

--- TijekIzvodjenja/TijekIzvođenja.cs
+++ TijekIzvodjenja/TijekIzvođenja.cs
@@ -10,17 +10,31 @@
 
         public static void IspisTryCatchFinally(int djeljenik, int djeljitelj)
         {
-            // TODO:030 Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
-            // TODO:031 Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
-            // TODO:032 Pokrenuti program i provjeriti što će se ispisati.
+            // Donje naredbu umetnuti u try-blok te dodati catch blok za hvatanje iznimke u slučaju dijeljenja s 0 i finally blok. 
+            // Dodati kontrolne ispise gornjih poruka u svaki od tih blokova.
+            // Pokrenuti program i provjeriti što će se ispisati.
+            try
+            {
+                Console.WriteLine(ZapočinjeBlokTry);
+                Console.WriteLine($"{djeljenik} dijelim s {djeljitelj}");
+                Console.WriteLine(djeljenik / djeljitelj);
+                Console.WriteLine($"{djeljenik} sam podijelio s {djeljitelj}");
+                Console.WriteLine(ZavršavaBlokTry);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(BlokCatch);
 
-            Console.WriteLine($"{djeljenik} dijelim s {djeljitelj}");
-            Console.WriteLine(djeljenik / djeljitelj);
-            Console.WriteLine($"{djeljenik} sam podijelio s {djeljitelj}");
+            }
+
+            finally
+            {
+                Console.WriteLine(BlokFinally);
+            }
 
         }
 
-        // TODO:033 Pokrenuti i provjeriti rezultate testova (2 testa iz grupe "TijekIzvođenja" moraju proći).
+        // Pokrenuti i provjeriti rezultate testova (2 testa iz grupe "TijekIzvođenja" moraju proći).
 
         static void Main()
         {
